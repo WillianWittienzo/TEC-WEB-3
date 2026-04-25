60d6f47cea05f70a24cf339ae1eab04370566aaf
WILLIAN WITTIENZO BISSIATO SILVA <wwittienzo@gmail.com>
2026-04-25 01:10:34 +0000

    Essa Porra mesmo

---
+++ tec-web/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Teams\TeamInvitationController;
+use App\Http\Middleware\EnsureTeamMembership;
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+
+Route::inertia('/', 'welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+])->name('home');
+
+Route::prefix('{current_team}')
+    ->middleware(['auth', 'verified', EnsureTeamMembership::class])
+    ->group(function () {
+        Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('invitations/{invitation}/accept', [TeamInvitationController::class, 'accept'])->name('invitations.accept');
+});
+
+Route::get('/novo/', function(){
+    return view('novo');
+});
+
+Route::get('/usuarios',[UserController::class, 'index']);
+
+require __DIR__.'/settings.php';
